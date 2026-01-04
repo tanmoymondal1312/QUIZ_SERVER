476f2704803a7e91195c378183a6d65cfb02e942
Tanmoy Mondal <tanmoymondal1315@gmail.com>
2026-01-04 22:22:02 +0600

    Settings Updated

--- QUIZ/settings.py
+++ QUIZ/settings.py
@@ -150,87 +150,89 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 """Production settings for QUIZ project."""
 
+"""
 
 
-# import os
-# from pathlib import Path
-
-# # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# # Quick-start development settings - unsuitable for production
-# # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+import os
+from pathlib import Path
 
-# # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-ov5d4$+6ex#4w(zh+g$ec7xw!kq5v92$u88tqio#z#4f_#q3&m'
-
-# # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-
-# ALLOWED_HOSTS = ["mcqsolvear.com", "www.mcqsolvear.com", "localhost", "127.0.0.1"]
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# # Application definition
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# INSTALLED_APPS = [
-#     'django.contrib.admin',
-#     'django.contrib.auth',
-#     'django.contrib.contenttypes',
-#     'django.contrib.sessions',
-#     'django.contrib.messages',
-#     'django.contrib.staticfiles',
-#     'django_extensions',
-#     'accounts',
-#     'app_quiz',
-#     'rest_framework.authtoken',
-#     'rest_framework',
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-ov5d4$+6ex#4w(zh+g$ec7xw!kq5v92$u88tqio#z#4f_#q3&m'
 
-# ]
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
 
-# MIDDLEWARE = [
-#     'django.middleware.security.SecurityMiddleware',
-#     'django.contrib.sessions.middleware.SessionMiddleware',
-#     'django.middleware.common.CommonMiddleware',
-#     'django.middleware.csrf.CsrfViewMiddleware',
-#     'django.contrib.auth.middleware.AuthenticationMiddleware',
-#     'django.contrib.messages.middleware.MessageMiddleware',
-#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-# ]
-
-# # Uncomment the following lines when deploying to production with HTTPS
-
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# CSRF_TRUSTED_ORIGINS = [
-#     'https://mcqsolvear.com',
-#     'https://www.mcqsolvear.com',
-#  ]
+ALLOWED_HOSTS = ["mcqsolvear.com", "www.mcqsolvear.com", "localhost", "127.0.0.1"]
 
 
-# ROOT_URLCONF = 'QUIZ.urls'
+# Application definition
 
-# TEMPLATES = [
-#     {
-#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [BASE_DIR / 'templates'], 
-#         'APP_DIRS': True,
-#         'OPTIONS': {
-#             'context_processors': [
-#                 'django.template.context_processors.request',
-#                 'django.contrib.auth.context_processors.auth',
-#                 'django.contrib.messages.context_processors.messages',
-#             ],
-#         },
-#     },
-# ]
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_extensions',
+    'accounts',
+    'app_quiz',
+    'rest_framework.authtoken',
+    'rest_framework',
 
-# WSGI_APPLICATION = 'QUIZ.wsgi.application'
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# Uncomment the following lines when deploying to production with HTTPS
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+CSRF_TRUSTED_ORIGINS = [
+    'https://mcqsolvear.com',
+    'https://www.mcqsolvear.com',
+ ]
 
 
-# # Database
-# # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+ROOT_URLCONF = 'QUIZ.urls'
 
-# settings.py
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'], 
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'QUIZ.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
 
 
 DATABASES = {
@@ -242,52 +244,53 @@ DATABASES = {
 
 
 
-# # Password validation
-# # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+# Password validation
+# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
-# AUTH_PASSWORD_VALIDATORS = [
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-#     },
-# ]
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 
-# # Internationalization
-# # https://docs.djangoproject.com/en/5.2/topics/i18n/
+# Internationalization
+# https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-us'
 
-# TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC'
 
-# USE_I18N = True
+USE_I18N = True
 
-# USE_TZ = True
-
-
-# # Static files (CSS, JavaScript, Images)
-# # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-# STATIC_URL = '/static/'
-# STATIC_ROOT = '/var/www/QUIZ_SERVER/static/'
-
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = '/var/www/QUIZ_SERVER/media/'
+USE_TZ = True
 
 
-# # Default primary key field type
-# # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/QUIZ_SERVER/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/var/www/QUIZ_SERVER/media/'
 
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
+
+
+"""
